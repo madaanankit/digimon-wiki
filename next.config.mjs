cf9b6e33a8542c6ccff6ae5d6bba7666b134a1ec
Ankit Madaan <ankit.ankit716@gmail.com>
2024-08-10 18:44:00 +0530

    digimon app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+export default {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'digimon.shadowsmith.com',
+                port: '',
+                pathname: '/img/**',
+            },
+        ],
+    },
+};
